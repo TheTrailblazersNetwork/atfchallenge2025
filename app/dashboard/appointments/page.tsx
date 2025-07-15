433b1a34837dce3cd4553ae92914933939022128
chemist-god <godtenkorang72@gmail.com>
2025-07-15 03:32:16 +0000

    setup project structure

---
+++ app/dashboard/appointments/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        Appointment
+    </div>
+  )
+}
+
+export default page
---
+++ app/dashboard/patients/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Patient
+    </div>
+  )
+}
+
+export default page
