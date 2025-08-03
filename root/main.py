5c05beb75e17486f2fe8dd71e40538da2449c98d
Ghasty <ghastflic@protonmail.com>
2025-08-03 23:44:51 +0100

    Refactor patient status handling

--- root/main.py
+++ root/main.py
@@ -124,7 +124,7 @@ async def sort(patients: List[Patient]):
                 patient["status"] = "APPROVED"
                 patient["scheduled_date"] = next_available_thursday_date
             else:
-                patient["status"] = "REBOOK"
+                patient["status"] = "RESCHEDULE"
         return {"results": patient_queue["results"]}
 
     except requests.exceptions.RequestException as e:
