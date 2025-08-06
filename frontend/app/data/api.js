43a26aa489ba0e8dc94f133445626e046f57b6d2
Anthony Saah <thepapisogram@gmail.com>
2025-08-06 12:31:01 +0000

    fixed missing api.js file

---
+++ frontend/app/data/api.js
@@ -0,0 +1,18 @@
+const server = {
+    remote: "https://ourbackend.com",
+    local: "http://localhost:5000",
+}
+const host = process.env.NODE_ENV === "production" ? server.remote : server.local;
+
+const system_api = {
+    patient: {
+        login: `${host}/api/auth/login`,
+        signup: `${host}/api/auth/signup`,
+        getData: `${host}/api/patient/`,
+        forgotPassword: `${host}/api/auth/forgot-password`,
+        // /:id added to the end of the below URLs
+        validateForgotPassword: `${host}/api/auth/reset-password/`,
+        resetPassword: `${host}/api/auth/reset-password/`,
+    },
+};
+export default system_api;
