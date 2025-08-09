ca46f1ab13636dfd161f6d0a847195438f8f465d
Anthony Saah <thepapisogram@gmail.com>
2025-08-09 16:39:56 +0000

    Added Components
    Added Page Components; pageFull, pageError, pageLoader...

--- frontend/components/signup/signup-form.tsx
+++ frontend/components/signup/signup-form.tsx
@@ -88,9 +88,7 @@ export function SignupForm({
           toast.success("Successful! Verify your email and mobile number.", {
             richColors: true,
           });
-          // uncomment below to continue work on verification
-          // localStorage.setItem("isTemp", "true");
-          // localStorage.setItem("tempId", res.data.message)
+          localStorage.setItem("verificationId", res.data.verificationId);
           router.push(`/signup/verify/`);
         } else {
           toast.error("Signup failed. Please try again.", { richColors: true });
