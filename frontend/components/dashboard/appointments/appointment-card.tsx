c10305d2acb7739d2fb00c5b9a57dc00f1e5095b
Anthony Saah <thepapisogram@gmail.com>
2025-08-09 01:09:37 +0000

    saving before bed

--- frontend/components/dashboard/appointments/appointment-card.tsx
+++ frontend/components/dashboard/appointments/appointment-card.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { AppointmentCardType } from "@/types/Appointment";
 import clsx from "clsx";
 import { Info } from "lucide-react";
+import { useState } from "react";
 
 const AppointmentCard = ({
   appointment,
