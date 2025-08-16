8be3beb12b01566af242ac68d347f27767b3cdd5
Anthony Saah <thepapisogram@gmail.com>
2025-08-16 23:02:48 +0000

    Build Error Fixed: Try #1

--- frontend/app/dashboard/patient/page.tsx
+++ frontend/app/dashboard/patient/page.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-
 import DashboardPageHeader from "@/components/dashboard/page-header";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
